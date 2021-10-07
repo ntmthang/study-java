453ead61b1def2c2fa116afa8901828830c40e2d
ntmthang <nguyentheminhthang@gmail.com>
2021-10-07 23:21:53 +0700

    Init project

---
+++ java/src/main/java/ptit/edu/java/App.java
@@ -0,0 +1,13 @@
+package ptit.edu.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
